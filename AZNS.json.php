bc9018d8bbdd275cd073af4e4ba800964ca9ace9
Azerpro1 <45076303+Azerpro1@users.noreply.github.com>
2023-05-25 13:14:57 +0400

    Create AZNS.json.php

---
+++ AZNS.json.php
@@ -0,0 +1,1 @@
+{"version":"6.0","changelog":"\n \u2705 Base update 278.0.0.21.117\n \u2705 Anti-Screenshot tracker\n \u2705 Anti-Reply viewing images\n \u2705 Hide that you listen to the voice messages\n \u2705 All the new Instagram features\n \u2705 A shortcut button for AzerProMODs on the main screen of Instagram.\n ","Burnt-version":"4.0","link":"https:\/\/wplusyukle.com"}
